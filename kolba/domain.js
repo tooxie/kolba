405bb5b9ffa4e37b2537f87281999093b95d662a
Alvaro Mouriño <alvaro@mourino.net>
2014-01-12 15:47:16 +0100

    Refactored Domain module

--- kolba/domain.js
+++ kolba/domain.js
@@ -1,20 +1,19 @@
 var domainModule = require('domain');
 
 function Domain() {
-    var domain = domainModule.create();
-
-    this.onError = function(callback) {
-        domain.on('error', callback);
-    };
-
-    this.run = function(callback) {
-        // domain.run(domain.bind(callback));
-        domain.run(callback);
-    };
-
-    this.add = function(obj) {
-        domain.add(obj);
-    };
+    this.domain = domainModule.create();
 }
+
+Domain.prototype.onError = function(callback) {
+    this.domain.on('error', callback);
+};
+
+Domain.prototype.run = function(callback) {
+    this.domain.run(callback);
+};
+
+Domain.prototype.add = function(obj) {
+    this.domain.add(obj);
+};
 
 module.exports = Domain;
